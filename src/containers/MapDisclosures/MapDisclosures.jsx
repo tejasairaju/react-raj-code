4df5db2360f13bdc3eb0d5da0c1ca94c5a941b16
Vinoth Kumar Venkata Subbu <vinothkumarsubbu@gmail.com>
2022-10-02 16:11:04 +0530

    minor changes

--- src/containers/MapDisclosures/MapDisclosures.jsx
+++ src/containers/MapDisclosures/MapDisclosures.jsx
@@ -32,8 +32,6 @@ const MapDisclosures = () => {
 
 
     useEffect(() => {
-        // getframeworkDetails();
-        // getDisclosures();
         getframeworks();
     }, []);
 
@@ -46,7 +44,6 @@ const MapDisclosures = () => {
                 setKCradioType(value.id)
             }
         }
-        // setCatagoryType(type+"__"+value.id);
         else if (type === "disclousure") {
             if (plane == "parent") {
                 setDPradioType(value.id)
@@ -58,20 +55,16 @@ const MapDisclosures = () => {
         }
     };
 
+    const onCanceltHandler = async() =>{
+        setParentDisclosureData([])
+        setChildDisclosureData([])
+        setParentKPI([])
+        setChildKPI([])
+    }
+
     const onNextHandler = async () => {
         try {
             setStatusData({ type: 'loading', message: '' });
-            // postdata = {}
-            // postdata["source_framework"]=sourceFrameworkId
-            // postdata["source_disclosure"]=radioDPType
-            // postdata["source_disclosure_kpi"]=radioKPType
-
-            // postdata["target_framework"]=destinationFrameworkId
-            // postdata["target_disclosure"]=radioDCType
-            // postdata["target_disclosure_kpi"]=radioKCType
-
-            // postdata["name"]=radioKPType+"___"+radioKCType
-
             const response = await axios.post(`${process.env.API_BASE_URL}/esgadmin/disclosure-mappings`, {
                 "source_framework": sourceFrameworkId,
                 "source_disclosure": radioDPType,
@@ -148,7 +141,6 @@ const MapDisclosures = () => {
     const onFrameworkSelect = async (val = "", type = "") => {
         console.log(val);
         getDisclosures('469652cb-dfc8-4d72-97b2-a5bd438f5e6e', type)
-        // getDisclosures(val.id, type)
     }
 
     const onCloseHandler = () => {
@@ -157,9 +149,9 @@ const MapDisclosures = () => {
     const headers = ['Name', 'Description', 'Action'];
     const radioButton = ['Environmental', 'Social', 'Goverance', 'General'];
 
-    const renderFrameworkLogo = (<div class="frameworks__choose">
+    const renderFrameworkLogo= (type) => (<div class="frameworks__choose">
         {(frameworksData || []).map((val, index) => (<div class={`frameworks__choose-item ${val.select ? 'active' : null}`}>
-            <img src={val.logo ? val.logo : `assets/images/avatar.jpg`} alt="GRI" onClick={() => onFrameworkSelect(val, "parent")} />
+            <img src={val.logo ? val.logo : `assets/images/avatar.jpg`} alt="GRI" onClick={() => onFrameworkSelect(val, type)} />
         </div>))
         }
     </div>)
@@ -177,7 +169,7 @@ const MapDisclosures = () => {
                         Choose a Framework to map from:
                     </h1>
 
-                    {renderFrameworkLogo}
+                    {renderFrameworkLogo('parent')}
 
                     <h1 class="assign__title">
                         Categories:
@@ -228,7 +220,7 @@ const MapDisclosures = () => {
                             {(parentKPI || []).map((val, index) => (
                                 <div class="disclosures__item">
                                     <p class="disclosures__detalis">
-                                        {val.code + " " + val.name}
+                                        {val.code + " " + val.label}
                                     </p>
                                     <label for="organisational__checkbox" class="disclosures__label" onClick={() => radioChangeHandler("kpi", val, "parent")}>
                                         <input type="checkbox"
@@ -242,51 +234,6 @@ const MapDisclosures = () => {
                                     </label>
                                 </div>)
                             )}
-                            {/* <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-1 Organisational details
-                                </p>
-                                <label for="1organisational__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1organisational__checkbox" />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-6 Activities, value chain and other business relationships
-                                </p>
-                                <label for="1activities__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1activities__checkbox" checked />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    22-9 Governance structure and composition 
-                                </p>
-                                <label for="1governance__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1governance__checkbox" />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-19 Remuneration policies
-                                </p>
-                                <label for="1remuneration__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1remuneration__checkbox" checked />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-23 GRI 305 -5 Reduction of GHG Emissions
-                                </p>
-                                <label for="1gri__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1gri__checkbox" checked />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div> */}
                         </div>
                     </div>
                 </div>
@@ -294,14 +241,7 @@ const MapDisclosures = () => {
                     <h1 class="assign__title">
                         Choose a Framework to map to:
                     </h1>
-                    {renderFrameworkLogo}
-                    {/* <div class="frameworks__choose-item active">
-                            <img src="./assets/images/sasb.png" alt="GRI" />
-                        </div>
-                        <div class="frameworks__choose-item">
-                            <img src="./assets/images/tcfd.png" alt="GRI" />
-                        </div> */}
-
+                    {renderFrameworkLogo('child')}
                     <h1 class="assign__title">
                         Categories:
                     </h1>
@@ -348,57 +288,11 @@ const MapDisclosures = () => {
                             Question:
                         </h1>
                         <div class="disclosures__wrapper">
-                            {/* <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-1 Organisational details
-                                </p>
-                                <label for="1organisational__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1organisational__checkbox" />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-6 Activities, value chain and other business relationships
-                                </p>
-                                <label for="1activities__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1activities__checkbox" checked />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    22-9 Governance structure and composition 
-                                </p>
-                                <label for="1governance__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1governance__checkbox" />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-19 Remuneration policies
-                                </p>
-                                <label for="1remuneration__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1remuneration__checkbox" checked />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div>
-                            <div class="disclosures__item">
-                                <p class="disclosures__detalis">
-                                    2-23 GRI 305 -5 Reduction of GHG Emissions
-                                </p>
-                                <label for="1gri__checkbox" class="disclosures__label">
-                                    <input type="checkbox" class="disclosures__checkbox" id="1gri__checkbox" checked />
-                                    <div class="fake__checkbox"></div>
-                                </label>
-                            </div> */}
-
-
+                           
                             {(childKPI || []).map((val, index) => (
                                 <div class="disclosures__item">
                                     <p class="disclosures__detalis">
-                                        {val.code + " " + val.name}
+                                        {val.code + " " + val.label}
                                     </p>
                                     <label for="organisational__checkbox" class="disclosures__label" onClick={() => radioChangeHandler("kpi", val, "child")}>
                                         <input type="checkbox"
@@ -418,7 +312,7 @@ const MapDisclosures = () => {
             </div>
             <div class="buttons__panel">
             
-                <Button label='CANCEL' onClickHandler={onNextHandler} className='buttons__panel-button' />
+                <Button label='CANCEL' onClickHandler={onCanceltHandler} className='buttons__panel-button' />
                 <Button label='MAP QUESTION' onClickHandler={onNextHandler} className='main__button map-disc-main-btn' />
             </div>
         </>)
