f24553c93d87b818105ffc4be9c47c68b3cc13ea
Vinoth Kumar Venkata Subbu <vinothkumarsubbu@gmail.com>
2022-10-28 00:45:48 +0530

    commited the mock Overlapping.

---
+++ src/containers/OverlappingDisclosures/OverlappingDisclosures.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import './OverlappingDisclosures.css';
+
+const OverlappingDisclosures = ({ component = '' }) => {
+
+    return (<>
+
+        <main class="main">
+            <div class="main__top-wrapper">
+                <h1 class="main__title_intelligent">
+                    <b>Intelligent Mapping</b>
+                </h1>
+            </div>
+            <div class="intelligent-framework">
+                <div class="system_admin_container_intelligent intelligent_main">
+
+                    <div class="framework__col-wrapper">
+                        <div class="intelligent_boxvalue">
+                            <div>
+                                <img src="./assets/images/gri.png" class="recommended_framework_intellgent" />
+                            </div>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent"><b>2.1 Organizational details</b></h2>
+                        </div>
+                    </div>
+
+                    <div class="framework__col-wrapper">
+                        <div class="intelligent_boxvalue">
+                            <div>
+                                <img src="./assets/images/sasb.png" class="recommended_framework_intellgent" />
+                            </div>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent"><b>CO-2 Give a general description and introdution
+                                to your
+                                organization</b></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="intelligent_content_wrapper">
+                <div class="intelligent_mapping_wrapper ">
+
+                    <div class="framework__row-wrapper main_title_bottom_mapping main_title_top_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2-1-a. report its legal name</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-a. Legal name </h2>
+                        </div>
+                    </div>
+                    <div class="framework__row-wrapper main_title_bottom_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2.1-b. report nature of ownership and legal form</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-b. Nature of ownership and legal form </h2>
+                        </div>
+                    </div>
+                    <div class="framework__row-wrapper main_title_bottom_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2.1-c. report the location of its headquarters</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-c. Location of its headquarters</h2>
+                        </div>
+                    </div>
+                    <div class="framework__row-wrapper main_title_bottom_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2.1-d. report its countries of operation</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-d. Countries of operation </h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="main__top-wrapper">
+                    <div class="framework__row">
+                        <h2 class="main__title_intelligent"><b>2-3 Reporting period, frequency and contact poiint</b>
+                        </h2>
+                    </div>
+                    <div class="framework__row">
+                        <h2 class="main__title_intelligent"><b>CO-2 State the start and end date of the year of which
+                            you for
+                            which you are reporting data</b></h2>
+                    </div>
+                </div>
+
+                <div class="intelligent_mapping_wrapper">
+
+                    <div class="framework__row-wrapper main_title_bottom_mapping  main_title_top_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2-1-a. report its legal name</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-a. Legal name </h2>
+                        </div>
+                    </div>
+                    <div class="framework__row-wrapper main_title_bottom_mapping ">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2.1-b. report nature of ownership and legal form</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-b. Nature of ownership and legal form </h2>
+                        </div>
+                    </div>
+                    <div class="framework__row-wrapper main_title_bottom_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2.1-b. report nature of ownership and legal form</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-b. Nature of ownership and legal form </h2>
+                        </div>
+                    </div>
+                    <div class="framework__row-wrapper main_title_bottom_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2.1-b. report nature of ownership and legal form</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-b. Nature of ownership and legal form </h2>
+                        </div>
+                    </div>
+                    <div class="framework__row-wrapper main_title_bottom_mapping">
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">2.1-b. report nature of ownership and legal form</h2>
+                        </div>
+                        <div class="framework__row">
+                            <h2 class="main__title_intelligent">CO-2-b. Nature of ownership and legal form </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+
+    </>)
+}
+export default OverlappingDisclosures;
