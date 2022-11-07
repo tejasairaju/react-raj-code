2a06250ebcfc11bc1f1a54c0f02cdd76beba7068
Vinoth Kumar Venkata Subbu <vinothkumarsubbu@gmail.com>
2022-11-07 20:38:12 +0530

    file download

--- src/Components/MoreAction/MoreAction.jsx
+++ src/Components/MoreAction/MoreAction.jsx
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
 import './MoreAction.css';
+import axios from "axios";
 
 const MoreAction = (props) => {
     const { value, index, viewListFramework = false, viewDisclosures = false, viewBespokeFramework=false,viewBespokeDisclosures=false,viewReport=false, ...rest } = props;
     const [isOpen, setIsopen] = useState(false);
+    const { orgDetails = {} } = useSelector(state => state.signup);
+    const { loginDetails = {} } = useSelector(state => state.signup);
     const navigate = useNavigate();
     const onNavigateHandler = (url) => {
         setIsopen(false);
         navigate(url);
+    }
+
+    const generateReport = async (id , name) => {
+        try {
+            axios.post(
+                `${process.env.API_BASE_URL}/reports/${id}/publish?organization=${orgDetails.name}`, 
+                {responseType: 'blob'} // !!!
+              ).then((response) => {
+                // window.open(URL.createObjectURL(new Blob(response.data)));
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', name+'.pdf'); //or any other extension
+                document.body.appendChild(link);
+                link.click();
+              })
+            
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     const onRedirectWithState = (url) => {
@@ -37,7 +61,7 @@ const MoreAction = (props) => {
                 <>
                     <div onClick={() => onRedirectWithState(`/template?isEditable=true`)}><a>Edit Framework</a></div>
                     {/* <div onClick={() => onRedirectWithState(`/template/${value.id}/bespoke/disclosures`)}><a>View Disclosures</a></div> */}
-                    <div onClick={() => onRedirectWithState(`/view/bespoke/${value.id}/disclosures`)}><a>View Disclosures</a></div>
+                    <div onClick={() => onRedirectWithState(`/view/bespoke/${value.id}/disclosures`, value.name)}><a>View Disclosures</a></div>
                     
                 </>
             }
@@ -52,6 +76,7 @@ const MoreAction = (props) => {
                 viewReport && 
                 <>
                     <div onClick={() => onRedirectWithState(`/report/${value.id}/disclosures`)}><a>Assign Disclosures</a></div>
+                    <div onClick={() => generateReport(value.id, value.name)}><a>Generate Reports</a></div>
                 </>
             }
         </div>
