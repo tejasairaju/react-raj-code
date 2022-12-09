73b9cfebea8b13f99392164273173ff9437afb53
Arjunan <rajarjunan@gmail.com>
2022-12-09 21:59:03 +0530

    Css cahnges for Client admin

--- src/containers/ViewMyTaskList/ViewMyTaskList.jsx
+++ src/containers/ViewMyTaskList/ViewMyTaskList.jsx
@@ -73,7 +73,7 @@ const ViewMyTaskList = (props) => {
                                 <td>{getDataFormat(report.start_date)}</td>
                                 <td>{getDataFormat(report.end_date)}</td>
                                 <td>{(report.status=="Custom")?"Bespoke":report.status}</td>
-                                <td><a onClick={() => redirectToViewDisclosures(report)} className={`answer-redirect-link`}>View Disclosures</a></td>
+                                <td><a onClick={() => redirectToViewDisclosures(report)} className={`answer-redirect-link cursor-pointer`}>View Disclosures</a></td>
                             </tr>);
                             // }
                         })}
